0f51aff017c61e5364a58bae81deea98b115ba5c
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2023-05-16 17:10:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-const User = require("../friendbook/models/User");
 
-User.findByID(1, (error, user) =>
-{
-    if (error)
-    {
-        console.log(error);
-        return;
-    };
-    console.log(user);
-});
